78f995ebd2b2c9520b707598e350507c9145eaf5
akramov_developer <bahrom.akramov98@gmail.com>
2023-01-27 00:37:09 +0500

    Add project files.

---
+++ Bahrom_Akramov/Lesson_26_January_2023/HomeTasks/Intersection_of_Two_Linked_Lists_160.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+
+namespace Bahrom_Akramov.Lesson_26_January_2023.HomeTasks
+{
+    internal class Intersection_of_Two_Linked_Lists_160
+    {
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+            ListNode listA = headA;
+            ListNode listB = headB;
+            Hashtable hashtable = new Hashtable();
+            int i = 1;
+            while (listB != null)
+            {
+                hashtable.Add(listB, i);
+                listB = listB.next;
+                i++;
+            }
+            int j = 0;
+            while (listA != null)
+            {
+                if (hashtable.ContainsKey(listA))
+                {
+                    break;
+                }
+                listA = listA.next;
+                j++;
+            }
+            return listA;
+        }
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+    }
+}
---
+++ Bahrom_Akramov/Lesson_26_January_2023/HomeTasks/Remove_Duplicates_from_Sorted_List_83.cs
@@ -0,0 +1,40 @@
+﻿namespace Bahrom_Akramov.Lesson_26_January_2023.HomeTasks
+{
+    internal class Remove_Duplicates_from_Sorted_List_83
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            if (head == null)
+            {
+                return head;
+            }
+            ListNode current = head;
+            ListNode next = head.next;
+            while (next != null)
+            {
+                if (current.val == next.val)
+                {
+                    current.next = next.next;
+                    next = next.next;
+                }
+                else
+                {
+                    current = next;
+                    next = next.next;
+                }
+            }
+            return head;
+
+        }
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+    }
+}
