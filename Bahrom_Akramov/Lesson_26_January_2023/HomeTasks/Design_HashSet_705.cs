5b55f0571e64dc5a770b0f985215755e8bcaeee4
akramov_developer <bahrom.akramov98@gmail.com>
2023-01-27 08:47:39 +0500

    picture of task4

--- Bahrom_Akramov/Lesson_26_January_2023/HomeTasks/Design_HashSet_705.cs
+++ Bahrom_Akramov/Lesson_26_January_2023/HomeTasks/Design_HashSet_705.cs
@@ -8,25 +8,28 @@ namespace Bahrom_Akramov.Lesson_26_January_2023.HomeTasks
 {
     internal class Design_HashSet_705
     {
-        List<int> list = new List<int>();
+        private Dictionary<int, int> dict;
         public Design_HashSet_705()
         {
-
+            dict = new Dictionary<int, int>();
         }
 
         public void Add(int key)
         {
-
+            if (!dict.ContainsKey(key))
+            {
+                dict.Add(key, 0);
+            }
         }
 
         public void Remove(int key)
         {
-
+            dict.Remove(key);
         }
 
         public bool Contains(int key)
         {
-            return true;
+            return dict.ContainsKey(key);
         }
     }
 }
