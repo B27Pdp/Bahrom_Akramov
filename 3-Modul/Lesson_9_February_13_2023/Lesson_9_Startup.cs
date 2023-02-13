21531b0d8ba9f3af0908363de4e6866de39a1e3d
akramov_developer <bahrom.akramov98@gmail.com>
2023-02-13 12:52:58 +0500

    Commit all

---
+++ 3-Modul/Lesson_9_February_13_2023/Lesson_9_Startup.cs
@@ -0,0 +1,18 @@
+﻿using _3_Modul.Lesson_9_February_13_2023.LessonTasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Modul.Lesson_9_February_13_2023
+{
+    internal class Lesson_9_Startup
+    {
+        public static void Run()
+        {
+            //XML_Example.Run();
+            XML_Example.Write();
+        }
+    }
+}
